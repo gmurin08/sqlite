585f9d9544c766d51128dba4dcf25b2705b39145
Gino Murin <gmurin@gmail.com>
2022-07-02 21:40:06 -0400

    created row struct for database

--- main.c
+++ main.c
@@ -8,6 +8,16 @@ The following code generates a simple Read Execute Print Loop
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdint.h>
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
+
+typedef struct{
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+} Row;
 
 typedef enum {
     META_COMMAND_SUCCESS,
@@ -20,6 +30,7 @@ typedef enum {STATEMENT_INSERT, STATEMENT_SELECT} StatementType;
 
 typedef struct {
     StatementType type;
+    Row row_to_insert; //only used by insert statement
 }Statement;
 
 typedef struct{
@@ -50,6 +61,12 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer){
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement){
     if (strncmp(input_buffer->buffer, "insert", 6) == 0){
         statement->type = STATEMENT_INSERT;
+        int args_assigned = sscanf(
+            input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
+            statement->row_to_insert.username, statement->row_to_insert.email);
+        if(args_assigned < 3){
+            return PREPARE_SYNTAX_ERROR;
+        }
         return PREPARE_SUCCESS;
     }
     if(strcmp(input_buffer->buffer, "select") == 0){
